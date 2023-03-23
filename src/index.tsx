640f089a7b9b83470b24300922922155743d45af
suneelponkam <104679140+suneelponkam@users.noreply.github.com>
2023-03-23 23:40:35 +0530

    style

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
-import theme from './theme';
+import theme from './styles/theme';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
